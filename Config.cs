12d41d4097d19a4a8772ed0ea3a9af890e480347
therealkanoah <ksawcio23456@gmail.com>
2025-08-10 00:23:15 +0200

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+using System.Text.Json.Serialization;
+
+namespace GiveWeapon
+{
+    public class GiveWeaponConfig : BasePluginConfig
+    {
+        [JsonPropertyName("Command")]
+        public string Command { get; set; } = "css_give";
+
+        [JsonPropertyName("AccessFlag")]
+        public string AccessFlag { get; set; } = "@css/root, @css/chat";
+    };
+}
